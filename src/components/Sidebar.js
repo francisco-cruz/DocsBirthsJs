850f24697e92f2e5456ac13ab3f9ff70f97905da
francisco-cruz <franaraujosilva2017@gmail.com>
2022-11-22 11:13:35 -0300

    aside

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,23 @@
-import { Flex } from "@chakra-ui/react";
-import { useMediaQuery } from '@chakra-ui/react'
+import {
+  Flex,
+  Image,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useMediaQuery } from "@chakra-ui/react";
+import Logo from "./Navbar/Logo";
 
 export default function Sidebar() {
-
-  const [isSmallerThan900] = useMediaQuery('(max-width: 767px)')
+  const [isSmallerThan900] = useMediaQuery("(max-width: 767px)");
 
   return (
     <>
       <Flex
-        display={isSmallerThan900 ? "none" : "block" }
-        position="fixed"
+        display={isSmallerThan900 ? "none" : "block"}
+        position="sticky"
         top="0"
         left="0"
         flexDir="column"
@@ -20,10 +28,80 @@ export default function Sidebar() {
         w="250px"
         bg="transparent"
       >
-        <Flex p="5%" flexDir="column" alignItems="flex-start" as="nav">
-          
+        <Flex
+          pl="16px"
+          pt="7px"
+          h="auto"
+          flexDir="column"
+          alignItems="flex-start"
+          as="header"
+          bg="red"
+        >
+          <Logo />
         </Flex>
+
+        <Stack mt="60px">
+          <Nav />
+        </Stack>
       </Flex>
     </>
   );
 }
+
+const Nav = () => {
+  return (
+    <Stack bg="#333">
+      {NAV_ITEMS.map((navItem) => (
+        <NavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const NavItem = ({ label, href }) => {
+  return (
+    <Stack
+      spacing={4}
+      display="flex"
+      flexDir="row-reverse"
+      justifyContent="start"
+      _hover={{
+        backgroundColor: "#61DAFB40",
+      }}
+    >
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? "#"}
+        justify={"space-between"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+        }}
+      >
+        <Text
+          ml="16px"
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
+          {label}
+        </Text>
+      </Flex>
+    </Stack>
+  );
+};
+
+const NAV_ITEMS = [
+  {
+    label: "Instalation",
+    href: "#",
+  },
+  {
+    label: "Components",
+    href: "#",
+  },
+  {
+    label: "How use",
+    href: "#",
+  },
+];
